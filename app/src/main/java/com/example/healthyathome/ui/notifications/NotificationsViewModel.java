67ec7618dc5700332b9662f4baef0b649b75568a
Maxwell Bruckhaus <maxwell.bruckhaus@gmail.com>
2020-10-23 23:15:42 -0700

    Add categories page

--- app/src/main/java/com/example/healthyathome/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/healthyathome/ui/notifications/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("Exercise categories");
     }
 
     public LiveData<String> getText() {
