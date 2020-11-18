808ffa77524ff3a64ab84c8a3648fd1856262740
Maxwell Bruckhaus <maxwell.bruckhaus@gmail.com>
2020-11-17 22:55:18 -0800

    Launch login activity, create mainActivity intent

---
+++ app/src/main/java/com/example/healthyathome/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.healthyathome;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/healthyathome/MainActivity.java
+++ app/src/main/java/com/example/healthyathome/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.healthyathome;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.healthyathome.ui.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
