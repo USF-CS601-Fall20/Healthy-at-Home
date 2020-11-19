1b7e359496b00f438cdfe4ddd5111cf3f4c8ea33
Maxwell Bruckhaus <maxwell.bruckhaus@gmail.com>
2020-11-18 20:28:39 -0800

    Store data in Cloud Firebase

--- app/src/main/java/com/example/healthyathome/RegisterActivity.java
+++ app/src/main/java/com/example/healthyathome/RegisterActivity.java
@@ -6,21 +6,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText mUserName, mEmail, mPassword, mPhone;
     private Button registerButton, alreadyRegButton;
     private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +44,7 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.registerButton);
         alreadyRegButton = findViewById(R.id.skipRegisterButton);
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         // If user is already logged in
         if (firebaseAuth.getCurrentUser() != null){
@@ -48,6 +59,8 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                String userName = mUserName.getText().toString();
+                String phoneNumber = mPhone.getText().toString();
 
                 if (TextUtils.isEmpty(email)){
                     mEmail.setError("No email provided");
@@ -71,6 +84,27 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
                             Toast.makeText(RegisterActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+
+                            // Get user ID and store data
+                            userID = firebaseAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+                            Map<String, Object> userMap = new HashMap<>();
+                            userMap.put("userName", userName);
+                            userMap.put("email", email);
+                            userMap.put("phone", phoneNumber);
+                            documentReference.set(userMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("TAG", "onSuccess: user profile is crated for " + userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("TAG", "onFailure: " + e.toString());
+                                }
+                            });
+
+                            // Start main activity
                             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                             startActivity(intent);
                         }else{
